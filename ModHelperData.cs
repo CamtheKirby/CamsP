da411bb6ed68025309f0d8a9e1ad4fd48cffd81e
Cam the Kirby <92703353+CamtheKirby@users.noreply.github.com>
2023-11-08 15:05:31 -0600

    Last Change

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,7 +6,7 @@ public static class ModHelperData
     public const string Version = "1.0.0";
     public const string Name = "Cams Paths";
 
-    public const string Description = "My Extra Paths! 2 New Banana Farm Paths and More Coming Soon!";
+    public const string Description = "DOWNLOAD CAM'S PACK INSTEAD OF THIS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! My Extra Paths! 2 New Banana Farm Paths and More Coming Soon!";
 
     public const string RepoOwner = "CamtheKirby"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "CamsP"; // TODO add your repo name here, also in the download url in README.md]
